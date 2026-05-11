8114b441d052568b3f02232565da6c3beeeadd19
jakem27 <jakewmarzetti@gmail.com>
2026-05-11 14:09:18 -0700

    login endpoint

--- project/Server/src/main/java/learn/watchlist/controllers/UserController.java
+++ project/Server/src/main/java/learn/watchlist/controllers/UserController.java
@@ -1,14 +1,18 @@
 package learn.watchlist.controllers;
 
 import learn.watchlist.domain.Result;
+import learn.watchlist.domain.ResultType;
 import learn.watchlist.domain.UserService;
 import learn.watchlist.models.User;
+import learn.watchlist.security.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
@@ -20,7 +24,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> create(@RequestBody User user) {
+    public ResponseEntity<?> create(@RequestBody User user) {
         Result<User> result = service.create(user);
 
         if(!result.isSuccess()) {
@@ -28,5 +32,20 @@ public class UserController {
         }
 
         return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        Result<User> result = service.authenticate(user);
+
+        if(result.isSuccess()) {
+            String token = JwtUtil.generateToken(user.getUsername());
+
+            return new ResponseEntity<>(Map.of("token", token), HttpStatus.OK);
+        } else if(result.getType() == ResultType.NOT_FOUND) {
+            return new ResponseEntity<>(result.getMessages(), HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(result.getMessages(), HttpStatus.UNAUTHORIZED);
+        }
     }
 }
