dc77eb917d8111b9d2e31f1d96a7b5988c501678
jakem27 <jakewmarzetti@gmail.com>
2026-05-11 11:00:22 -0700

    db set_known_good_state()

--- project/Server/database/test_schema.sql
+++ project/Server/database/test_schema.sql
@@ -10,3 +10,18 @@ create table user(
 	favorite_actor text null
 );
 
+
+
+delimiter //
+create procedure set_known_good_state()
+begin
+	delete from user;
+	alter table user auto_increment = 1;
+
+	insert into user(id, username, password) values
+	(1, "user1", "password"),
+	(2, "user2", "123");
+	
+end //
+
+delimiter ;
--- project/Server/src/test/java/learn/watchlist/data/UserJdbcClientRepositoryTest.java
+++ project/Server/src/test/java/learn/watchlist/data/UserJdbcClientRepositoryTest.java
@@ -1,9 +1,11 @@
 package learn.watchlist.data;
 
 import learn.watchlist.models.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.simple.JdbcClient;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,15 +13,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserJdbcClientRepositoryTest {
 
     @Autowired
+    JdbcClient jdbcClient;
+
+    @Autowired
     UserJdbcClientRepository repository;
+
+    @BeforeEach
+    void setup() { jdbcClient.sql("call set_known_good_state();").update(); }
 
     @Test
     void shouldAdd() {
         User user = new User();
-        user.setUsername("user1");
+        user.setUsername("user3");
         user.setPassword("password");
         User actual = repository.create(user);
 
-        assertEquals(1, actual.getId());
+        assertEquals(3, actual.getId());
     }
 }
