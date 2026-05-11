104373ae15535919e1f8129bc3f56130b5146c34
jakem27 <jakewmarzetti@gmail.com>
2026-05-11 13:46:44 -0700

    jwt util class

---
+++ project/Server/src/main/java/learn/watchlist/security/JwtUtil.java
@@ -0,0 +1,37 @@
+package learn.watchlist.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+    private static final String SECRET = "supersecretkeystring"; // move to environment variables
+
+    private static final Key KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    private static final long EXPIRATION = 1000 * 60 * 60 * 24; // 24 hours
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(KEY)
+                .compact();
+    }
+
+    public static boolean validate(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(KEY)
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+        } catch (JwtException ex) {
+            return false;
+        }
+    }
+}
