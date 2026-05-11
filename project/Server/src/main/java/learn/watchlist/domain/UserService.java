dd7b4182b5711cf9b9f666da4fda10345d0e5045
jakem27 <jakewmarzetti@gmail.com>
2026-05-11 14:18:41 -0700

    service authenticate user method + tests

--- project/Server/src/main/java/learn/watchlist/domain/UserService.java
+++ project/Server/src/main/java/learn/watchlist/domain/UserService.java
@@ -13,6 +13,24 @@ public class UserService {
         this.repository = repository;
     }
 
+    public Result<User> authenticate(User proposedUser) {
+        Result<User> result = new Result<>();
+
+        User userFromDb = repository.findByUsername(proposedUser.getUsername());
+        if(userFromDb == null) {
+            result.addMessage("User does not exist", ResultType.NOT_FOUND);
+            return result;
+        }
+
+        if(userFromDb.getPassword().equals(proposedUser.getPassword())) {
+            result.setPayload(userFromDb);
+        } else {
+            result.addMessage("Incorrect password", ResultType.INVALID);
+        }
+
+        return result;
+    }
+
     public Result<User> create(User user) {
         Result<User> result = new Result<>();
 
--- project/Server/src/test/java/learn/watchlist/domain/UserServiceTest.java
+++ project/Server/src/test/java/learn/watchlist/domain/UserServiceTest.java
@@ -78,6 +78,40 @@ class UserServiceTest {
         assertEquals("That username is taken", result.getMessages().get(0));
     }
 
+    @Test
+    void shouldLogin() {
+        User proposed = makeUser();
+
+        when(repository.findByUsername(proposed.getUsername())).thenReturn(makeUser());
+
+        Result<User> result = service.authenticate(proposed);
+        assertTrue(result.isSuccess());
+    }
+
+    @Test
+    void shouldNotLoginDoesNotExist() {
+        User proposed = makeUser();
+
+        when(repository.findByUsername(proposed.getUsername())).thenReturn(null);
+
+        Result<User> result = service.authenticate(proposed);
+        assertEquals(ResultType.NOT_FOUND, result.getType());
+        assertEquals("User does not exist", result.getMessages().get(0));
+    }
+
+    @Test
+    void shouldNotLoginIncorrectPassword() {
+        User proposed = makeUser();
+        User existing = makeUser();
+        existing.setPassword("different password");
+
+        when(repository.findByUsername(proposed.getUsername())).thenReturn(existing);
+
+        Result<User> result = service.authenticate(proposed);
+        assertEquals(ResultType.INVALID, result.getType());
+        assertEquals("Incorrect password", result.getMessages().get(0));
+    }
+
     private User makeUser() {
         User user = new User();
         user.setUsername("user123");
