4a7edbe4b5d18f419045498a5299f2e610756eda
jakem27 <jakewmarzetti@gmail.com>
2026-05-14 13:22:43 -0700

    MovieFolder model 2

---
+++ project/Server/src/main/java/learn/watchlist/models/MovieFolder.java
@@ -0,0 +1,42 @@
+package learn.watchlist.models;
+
+public class MovieFolder {
+    private int folderId;
+    private Movie movie;
+    private boolean watched;
+    private boolean liked;
+
+    public MovieFolder() {}
+
+    public int getFolderId() {
+        return folderId;
+    }
+
+    public void setFolderId(int folderId) {
+        this.folderId = folderId;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public boolean isWatched() {
+        return watched;
+    }
+
+    public void setWatched(boolean watched) {
+        this.watched = watched;
+    }
+
+    public boolean isLiked() {
+        return liked;
+    }
+
+    public void setLiked(boolean liked) {
+        this.liked = liked;
+    }
+}
