1068bca99f5e36f6ee412b2bf35c2f46c6b1ea08
jakem27 <jakewmarzetti@gmail.com>
2026-05-15 12:26:55 -0700

    friendship model

---
+++ project/Server/src/main/java/learn/watchlist/models/Friendship.java
@@ -0,0 +1,33 @@
+package learn.watchlist.models;
+
+public class Friendship {
+    private int user1Id;
+    private int user2Id;
+    private boolean isPending;
+    
+    public Friendship() {}
+
+    public int getUser1Id() {
+        return user1Id;
+    }
+
+    public void setUser1Id(int user1Id) {
+        this.user1Id = user1Id;
+    }
+
+    public int getUser2Id() {
+        return user2Id;
+    }
+
+    public void setUser2Id(int user2Id) {
+        this.user2Id = user2Id;
+    }
+
+    public boolean isPending() {
+        return isPending;
+    }
+
+    public void setPending(boolean pending) {
+        isPending = pending;
+    }
+}
