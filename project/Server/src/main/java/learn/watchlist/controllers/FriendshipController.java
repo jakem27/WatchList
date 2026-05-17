1c884d8f3c7812051a1a62f604263608a5512bf3
jakem27 <jakewmarzetti@gmail.com>
2026-05-17 11:59:45 -0700

    http requests

--- project/Server/src/main/java/learn/watchlist/controllers/FriendshipController.java
+++ project/Server/src/main/java/learn/watchlist/controllers/FriendshipController.java
@@ -4,7 +4,6 @@ import learn.watchlist.domain.FriendshipService;
 
 import learn.watchlist.domain.Result;
 import learn.watchlist.domain.ResultType;
-import learn.watchlist.models.Friendship;
 import learn.watchlist.models.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
