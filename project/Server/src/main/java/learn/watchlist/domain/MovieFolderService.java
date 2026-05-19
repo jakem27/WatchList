d606dc88582e8a3dca5e358b562d03d1e257c127
jakem27 <jakewmarzetti@gmail.com>
2026-05-18 18:49:42 -0700

    permit MovieFolder retrieval by friend when public

--- project/Server/src/main/java/learn/watchlist/domain/MovieFolderService.java
+++ project/Server/src/main/java/learn/watchlist/domain/MovieFolderService.java
@@ -1,13 +1,7 @@
 package learn.watchlist.domain;
 
-import learn.watchlist.data.FolderRepository;
-import learn.watchlist.data.MovieFolderRepository;
-import learn.watchlist.data.MovieRepository;
-import learn.watchlist.data.UserRepository;
-import learn.watchlist.models.Folder;
-import learn.watchlist.models.Movie;
-import learn.watchlist.models.MovieFolder;
-import learn.watchlist.models.User;
+import learn.watchlist.data.*;
+import learn.watchlist.models.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,17 +12,19 @@ public class MovieFolderService {
     private final UserRepository userRepository;
     private final FolderRepository folderRepository;
     private final MovieRepository movieRepository;
+    private final FriendshipRepository friendshipRepository;
 
-    public MovieFolderService(MovieFolderRepository movieFolderRepository, UserRepository userRepository, FolderRepository folderRepository, MovieRepository movieRepository) {
+    public MovieFolderService(MovieFolderRepository movieFolderRepository, UserRepository userRepository, FolderRepository folderRepository, MovieRepository movieRepository, FriendshipRepository friendshipRepository) {
         this.movieFolderRepository = movieFolderRepository;
         this.userRepository = userRepository;
         this.folderRepository = folderRepository;
         this.movieRepository = movieRepository;
+        this.friendshipRepository = friendshipRepository;
     }
 
     public Result<List<MovieFolder>> findByFolderId(int folderId, String username) {
         Result<List<MovieFolder>> result = new Result<>();
-        authenticateUser(result, username);
+        User user = authenticateUser(result, username);
 
         Folder folder = folderRepository.findById(folderId);
         if(folder == null) {
@@ -38,8 +34,16 @@ public class MovieFolderService {
 
         if(!username.equals(folder.getUser().getUsername())) {
             // check for friendship and public/private
-            result.addMessage("Cannot access someone else's folder", ResultType.INVALID);
-            return result;
+            if(!folder.isPublic()) {
+                result.addMessage("Folder is private", ResultType.INVALID);
+                return result;
+            }
+
+            Friendship friendship = friendshipRepository.findByUsers(folder.getUser().getId(), user.getId());
+            if(friendship == null || friendship.isPending()) {
+                result.addMessage("Cannot access someone else's folder if not friends", ResultType.INVALID);
+                return result;
+            }
         }
 
         List<MovieFolder> movieFolders = movieFolderRepository.findByFolderId(folderId);
@@ -93,11 +97,13 @@ public class MovieFolderService {
         return result;
     }
 
-    private void authenticateUser(Result<?> result, String username) {
+    private User authenticateUser(Result<?> result, String username) {
         User user = userRepository.findByUsername(username);
         if(user == null) {
             result.addMessage("Invalid user", ResultType.INVALID);
         }
+
+        return user;
     }
 
 }
